7ef10bcac53a7f722cd947a21fadd27e7cefca35
Thiago Carvalho <thiagopaes1993@gmail.com>
2023-10-01 20:01:32 -0300

    🎨 Snackbar copiado com sucesso

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@ import SelectList from "./components/SelectList.vue";
       </div>
       <SelectList :items="unitList" v-model="targetUnit" />
     </div>
+    <div id="snackbar">Copied to clipboard!</div>
   </div>
 </template>
 
@@ -78,6 +79,17 @@ export default {
   methods: {
     copy() {
       navigator.clipboard.writeText(this.outputValue);
+
+      // Get the snackbar DIV
+      var x = document.getElementById("snackbar");
+
+      // Add the "show" class to DIV
+      x.className = "show";
+
+      // After 3 seconds, remove the show class from DIV
+      setTimeout(function () {
+        x.className = x.className.replace("show", "");
+      }, 3000);
     },
 
     searchUnits(searchString) {
